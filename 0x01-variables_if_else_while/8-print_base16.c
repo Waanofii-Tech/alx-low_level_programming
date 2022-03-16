1911e8afedea74b1c1e673bb1347f8d9eab92da0
Waanofii-Tech <waanofii.tech@gmail.com>
2022-03-16 10:58:15 -0700

    added print base16 code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
