c5fca493a10ce53b56469359ea86ad68e8f5090e
Waanofii-Tech <waanofii.tech@gmail.com>
2022-03-16 09:37:04 -0700

    still fixing style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,10 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
 	printf("%i", n);
-	
 	if (n < 0)
 		printf(" is negative\n");
 	else if (n > 0)
