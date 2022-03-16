8921cae39292b1915a9f8afe54a7fa735fc45724
Waanofii-Tech <waanofii.tech@gmail.com>
2022-03-16 10:54:23 -0700

    added print numberz code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
