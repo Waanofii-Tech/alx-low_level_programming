c05cee7b5c521292d70bcbf6046d72bfb0ac2027
Waanofii-Tech <waanofii.tech@gmail.com>
2022-03-17 12:18:47 -0700

    uploaded all code file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * add - function that adds two integers together
+ * Return: a + b result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - function that prints the absolute value of a number
+ * Return: 1 if > 0, 0 if == 0, positive if negative
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
