4e2786ac54419b1a15031566f0d02d49bff7fd93
Waanofii-Tech <waanofii.tech@gmail.com>
2022-03-17 12:37:23 -0700

    fixed isalpha code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * isalpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
  * @c: takes in a character
  */
